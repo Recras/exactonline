8e11ac550bdc7aebcc4a0ffa037b4a6bb77bf7e1
Dirk Nederveen <dirk@recras.nl>
2016-04-28 12:18:07 +0200

    Statuspage: first setup, with some checks

--- handlers/manage.go
+++ handlers/manage.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+	"text/template"
 
 	"github.com/Recras/exactonline"
 	"github.com/Recras/exactonline/dal"
+	"github.com/Recras/exactonline/libhttp"
 	"github.com/Recras/exactonline/recras"
 	"github.com/Recras/exactonline/synctool"
 	"github.com/Sirupsen/logrus"
@@ -16,6 +18,18 @@ import (
 	"github.com/jmoiron/sqlx"
 	"golang.org/x/oauth2"
 )
+
+type administrationData struct {
+	RecrasBedrijfNaam string
+	Error             string
+	EverythingOK      bool
+
+	DefaultItemGroupOK   bool
+	DefaultItemGroupCode string
+
+	DefaultJournalOK   bool
+	DefaultJournalDesc string
+}
 
 func GetStatus(w http.ResponseWriter, r *http.Request) {
 	cookieStore := context.Get(r, "cookieStore").(*sessions.CookieStore)
@@ -30,26 +44,99 @@ func GetStatus(w http.ResponseWriter, r *http.Request) {
 	db := context.Get(r, "db").(*sqlx.DB)
 
 	parts := strings.Split(u.Email, "@")
-	//recras_username := parts[0]
 	recras_hostname := parts[1]
-	cred, err := dal.FindCredentialByRecrasHostname(db, recras_hostname)
-	if err != nil {
-		logrus.Infof("handlers.GetStatus: error retrieving credentials for `%s`: %s", recras_hostname, err)
-	}
 
-	cl := exactonline.EnvConfig().NewClient(oauth2.Token{RefreshToken: *cred.ExactRefreshToken})
-	err = cl.GetDefaultDivision()
-	if err != nil {
-		logrus.Errorf("handlers.GetStatus: error retrieving currentdivision: %s", err)
-	}
-	fmt.Fprintf(w, "CurrentDivision: %d\n", cl.Division)
+	logger := logrus.WithFields(logrus.Fields{
+		"function":        "handlers.GetStatus",
+		"recras_hostname": recras_hostname,
+	})
 
-	producten, err := cl.GetAllItems()
+	tmpl, err := template.ParseFiles("templates/dashboard.html.tmpl", "templates/status.html.tmpl")
 	if err != nil {
-		logrus.Errorf("handlers.GetStatus: error retrieving Items: %s", err)
+		libhttp.HandleErrorJson(w, err)
+		logger.Errorf("template parsing error %s", err)
 		return
 	}
-	fmt.Fprintf(w, "Producten: %s\n", producten)
+
+	data := struct {
+		CurrentUser     *dal.UserRow
+		Administrations []administrationData
+		GeneralError    string
+	}{
+		CurrentUser:     u,
+		Administrations: []administrationData{},
+	}
+
+	cred, err := dal.FindCredentialByRecrasHostname(db, recras_hostname)
+	if err != nil {
+		logger.Infof("error retrieving credentials: %s", err)
+	}
+	if cred.ExactRefreshToken == nil {
+		logger.Error("no ExactRefreshToken")
+		data.GeneralError = "Verbinding met Exact Online is nog niet gelegd"
+		w.WriteHeader(400)
+		tmpl.Execute(w, data)
+		return
+	}
+
+	tok := oauth2.Token{
+		RefreshToken: *cred.ExactRefreshToken,
+	}
+	cl := exactonline.EnvConfig().NewClient(tok)
+	err = cl.GetDefaultDivision()
+	if err != nil {
+		logger.Errorf("error retrieving currentdivision: %s", err)
+		w.WriteHeader(500)
+		data.GeneralError = "Verbindingsfout met Exact Online"
+		tmpl.Execute(w, data)
+		return
+	}
+
+	rcl := recras.NewClient(cred.RecrasHostname, cred.RecrasUsername, cred.RecrasPassword)
+	bedrijven, err := rcl.GetBedrijven(nil)
+	if err != nil {
+		logger.Errorf("error retrieving bedrijven: %s", err)
+		w.WriteHeader(500)
+		data.GeneralError = "Verbindingsfout met Recras"
+		tmpl.Execute(w, data)
+		return
+	}
+	for _, b := range bedrijven {
+		data.Administrations = append(data.Administrations, administrationData{
+			RecrasBedrijfNaam: b.Bedrijfsnaam,
+		})
+		adminStatus := &data.Administrations[len(data.Administrations)-1]
+
+		err = cl.SetDivisionByVATNumber(b.BTWNummer)
+		if err == exactonline.ErrDivisionNotFound {
+			adminStatus.Error = "Geen administratie in Exact Online met BTW-nummer " + b.BTWNummer
+			continue
+		}
+		bedrijflogger := logger.WithField("exact_administration_id", cl.Division)
+		bedrijflogger.Info("Bedrijf gevonden")
+
+		itemgroup, err := cl.FindDefaultItemGroup()
+		if err != nil {
+			bedrijflogger.Info(err.Error())
+		}
+		adminStatus.DefaultItemGroupOK = (err == nil)
+		adminStatus.DefaultItemGroupCode = itemgroup.Code
+
+		checkDagboek(cl, adminStatus, bedrijflogger)
+
+		adminStatus.EverythingOK = adminStatus.DefaultItemGroupOK && adminStatus.DefaultJournalOK
+	}
+
+	tmpl.Execute(w, data)
+}
+
+func checkDagboek(cl *exactonline.Client, ad *administrationData, logger *logrus.Entry) {
+	j, err := cl.FindDefaultJournal()
+	if err != nil && err != exactonline.ErrJournalNotFound {
+		logrus.Errorf("%#v", err)
+	}
+	ad.DefaultJournalOK = (err == nil)
+	ad.DefaultJournalDesc = j.Description
 }
 
 func SyncRecras(cred *dal.Credential, entry *logrus.Entry) {
