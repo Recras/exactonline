acad0b506f46091ba79d25861a13967a93d1d787
Dirk Nederveen <dirk@zwartgeel.nl>
2019-05-16 16:14:55 +0200

    Improve message for httperror.HTTPError.error()

--- httperror/httperror.go
+++ httperror/httperror.go
@@ -16,7 +16,7 @@ func (e HTTPError) Error() string {
 	defer e.Response.Body.Close()
 	body, err := ioutil.ReadAll(e.Response.Body)
 	if err != nil {
-		return "ioutil.ReadAll failed"
+		return fmt.Sprintf("ioutil.ReadAll failed, no response body, HTTP error %d for %s %s", e.StatusCode, e.Request.Method, e.Request.URL)
 	}
 	return fmt.Sprintf("HTTP error %d for %s %s: %s", e.StatusCode, e.Request.Method, e.Request.URL, body)
 }
